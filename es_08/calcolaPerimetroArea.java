a03d53a75ff1307c7817c1ff642a908b42c8775b
Eman95abcd <emanuele.kappa95@gmail.com>
2023-05-15 12:13:01 +0200

    07 inserito condizione if else

--- es_08/calcolaPerimetroArea.java
+++ es_08/calcolaPerimetroArea.java
@@ -2,19 +2,25 @@ import java.util.Scanner; //libreria che serve per poter utilizzare i metodi di 
 
 public class calcolaPerimetroArea // deve chiamarsi con il nome del file .java
 {
-    public static void main(String[] args)
-    {
-      Scanner input = new Scanner(System.in); //creo un oggetto Scanner chiamato input
-      System.out.print("Inserisci la lughezza del lato: ");
-      int lato; 
-      lato = input.nextInt(); 
-      int area;
-      int perimetro;
-      area = lato * lato;
-      perimetro = lato * 4;
-      System.out.println("Il lato inserito: " + lato); //concateno testo e variabili
-      System.out.println("L'area e': "+ area);
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in); // creo un oggetto Scanner chiamato input
+    System.out.print("Inserisci la lughezza del lato: ");
+    int lato;
+    lato = input.nextInt();
+    int area;
+    int perimetro;
+    area = lato * lato;
+    perimetro = lato * 4;
+    if (lato > 40) {      //aggiunto una condizione che verifica l'input
+      System.out.println("Hai inserito un numero troppo grande");   //se l'input é > 40
+
+    } else {       // se l'input é <= 40
+      System.out.println("Il lato inserito: " + lato); // concateno testo e variabili
+      System.out.println("L'area e': " + area);
       System.out.println("Il perimetro e': " + perimetro);
-      input.close(); 
-    
+    }
+    // lascio fuori dall'if else la chusura della classe input
+
+    input.close();
+  }
 }
