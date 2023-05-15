92f259d4cbc1013d2cbcb583ba87a6a157599be2
Eman95abcd <emanuele.kappa95@gmail.com>
2023-05-15 10:55:14 +0200

    03 cancellazione commento

--- es_08/calcolaPerimetroArea.java
+++ es_08/calcolaPerimetroArea.java
@@ -2,7 +2,7 @@ public class calcolaPerimetroArea // deve chiamarsi con il nome del file .java
 {
     public static void main(String[] args)
     {
-      // le istruzioni vanno scritte all'interno del metodo main
+      
       int lato; //questo sarà il parametro che utilizzerò per i calcoli 
       lato = 3;
     }
