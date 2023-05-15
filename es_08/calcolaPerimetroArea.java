afaf52b52628de4413c0319f49e55b2470faf2a0
Eman95abcd <emanuele.kappa95@gmail.com>
2023-05-15 11:50:13 +0200

    05 inserito prompt

--- es_08/calcolaPerimetroArea.java
+++ es_08/calcolaPerimetroArea.java
@@ -1,9 +1,13 @@
+import java.util.Scanner; //libreria che serve per poter utilizzare i metodi di Scanner (lettura input)
+
 public class calcolaPerimetroArea // deve chiamarsi con il nome del file .java
 {
     public static void main(String[] args)
     {
-      
-      int lato; //questo sarà il parametro che utilizzerò per i calcoli 
+      Scanner input = new Scanner(System.in); //creo un oggetto Scanner chiamato input
+      System.out.print("Inserisci la lughezza del lato: ");
+      int lato; //questo sarà il parametro che utilizzerò per i calcoli
+      lato = input.nextInt(); 
       int area;
       int perimetro;
       lato = 5; //assegno il valore 5 alla variabile lato
@@ -12,6 +16,7 @@ public class calcolaPerimetroArea // deve chiamarsi con il nome del file .java
       System.out.println(lato); //stampa della variabile lato
       System.out.println(area);
       System.out.println(perimetro);
+      input.close(); // la chiusura è opzionale ma segna in giallo l'input finchè non lo mettiamo
     }
     
 }
