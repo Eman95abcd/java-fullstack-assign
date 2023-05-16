94161238b656c9cd21843cf526760c2595be8ac0
Eman95abcd <emanuele.kappa95@gmail.com>
2023-05-16 09:29:03 +0200

    esercizio_11

---
+++ es_11/ArrayStr.java
@@ -0,0 +1,15 @@
+package esercizi.es_11;
+
+public class ArrayStr {
+    public static void main(String[] args) 
+    {
+        String[] words = {"uno","due","tre","quattro","cinque"};
+        for (String word : words)
+        {
+            
+            System.out.println(word);
+          
+        }
+    }
+    
+}
